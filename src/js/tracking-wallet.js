e606275c881b9ce4c1a80be2ab87184ad7c649bf
Edgar Cumbreras <edgarcumbrerastirado@gmail.com>
2016-03-02 12:56:35 +0100

    Unifies criteria on regex naming

--- src/js/tracking-wallet.js
+++ src/js/tracking-wallet.js
@@ -434,13 +434,13 @@
       var check = false;
 
       (function(a){
-        var A = new RegExp([
+        var deviceCategory = new RegExp([
           '(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|',
           'iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|',
           'palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|',
           'vodafone|wap|windows ce|xda|xiin|android|ipad|playbook|silko/i'].join(''));
 
-        var B = new RegExp([
+        var browserCategory = new RegExp([
           '1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|',
            'an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|',
            'br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|',
@@ -460,7 +460,7 @@
            'veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|',
            'whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i'].join(''))
 
-        if (A.test(a)||B.test(a.substr(0,4))) check = true
+        if (deviceCategory.test(a) || browserCategory.test(a.substr(0,4))) check = true
 
       })(navigator.userAgent || navigator.vendor || window.opera);
 
